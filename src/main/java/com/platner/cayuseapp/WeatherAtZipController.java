2ffbf48c88bf501bf2358434b220e83a08de4d63
cplatner <chris@platner.com>
2019-05-05 22:51:01 -0700

    - Lots of testing and internal fixes.  The API basically works as desired - Most of the tests I wanted are present

--- src/main/java/com/platner/cayuseapp/WeatherAtZipController.java
+++ src/main/java/com/platner/cayuseapp/WeatherAtZipController.java
@@ -4,9 +4,17 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -14,6 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/weather")
 public class WeatherAtZipController
 {
     @Value("${weatherapi.url}")
@@ -32,9 +41,14 @@ public class WeatherAtZipController
     private Map<String, String> timezones = new HashMap<>();
     private Map<String, String> elevations = new HashMap<>();
 
-    @GetMapping("/weather")
-    public WeatherMessage weather(@RequestParam(value = "zipcode") String zipcode)
+    @GetMapping
+    public ResponseEntity<?> weather(@RequestParam(value = "zipcode") String zipcode)
     {
+        ResponseEntity<?> checkZip = validateZipcode(zipcode);
+        if (checkZip.getStatusCode().isError()) {
+            return checkZip;
+        }
+
         ObjectMapper mapper = new ObjectMapper();
         SimpleRestTemplate restClient = new SimpleRestTemplate();
 
@@ -43,18 +57,23 @@ public class WeatherAtZipController
 
         //* Get weather
         String weatherUrl = String.format(weatherApiUri, zipcode, weatherApiAppId);
-        String s = restClient.get(weatherUrl);
+        ResponseEntity<?> response = get(weatherUrl);
+        if (response.getStatusCode().isError()) {
+            return response;
+        }
         try {
-            JsonNode root = mapper.readTree(s);
+            JsonNode root = mapper.readTree(response.getBody().toString());
             //* temperature is at main.temp
             temp = root.at("/main/temp").asText();
             city = root.at("/name").asText();
             lat = root.at("/coord/lat").asText();
             lon = root.at("/coord/lon").asText();
-        } catch (JsonParseException e) {
-            // return a 500
-        } catch (IOException e) {
-            //* return a 500
+        }
+        catch (JsonParseException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error parsing data from temperature server");
+        }
+        catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading data from temperature server");
         }
 
         //* Get timezonee
@@ -62,15 +81,17 @@ public class WeatherAtZipController
             timezone = timezones.get(zipcode);
         } else {
             String timezoneUrl = String.format(timezoneApiUrl, lat, lon, Instant.now().getEpochSecond(), timezoneApiAppId);
-            s = restClient.get(timezoneUrl);
+            String s = restClient.get(timezoneUrl);
             try {
                 JsonNode root = mapper.readTree(s);
                 timezone = root.at("/timeZoneName").asText();
                 timezones.put(zipcode, timezone);
-            } catch (JsonParseException e) {
-                // return a 500
-            } catch (IOException e) {
-                //* return a 500
+            }
+            catch (JsonParseException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error parsing data from temperature server");
+            }
+            catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading data from temperature server");
             }
         }
 
@@ -79,18 +100,64 @@ public class WeatherAtZipController
             elevation = elevations.get(zipcode);
         } else {
             String elevationUrl = String.format(elevationApiUrl, lat, lon, elevationApiAppId);
-            s = restClient.get(elevationUrl);
+            String s = restClient.get(elevationUrl);
             try {
                 JsonNode root = mapper.readTree(s);
                 elevation = root.at("/results/0/elevation").asText();
                 elevations.put(zipcode, elevation);
-            } catch (JsonParseException e) {
-                // return a 500
-            } catch (IOException e) {
-                //* return a 500
+            }
+            catch (JsonParseException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error parsing data from temperature server");
+            }
+            catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading data from temperature server");
             }
         }
 
-        return new WeatherMessage(city, temp, timezone, elevation);
+        return new ResponseEntity<>(new WeatherMessage(city, temp, timezone, elevation), HttpStatus.OK);
+    }
+
+    private ResponseEntity<?> get(String uri)
+    {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+
+        ResponseEntity<?> responseEntity = ResponseEntity.badRequest().build();
+
+        try {
+            responseEntity = rest.exchange(uri,
+                    HttpMethod.GET, new HttpEntity<>("", headers), String.class);
+        }
+        catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body("Error getting data from outside service");
+        }
+
+        return responseEntity;
+    }
+
+    private ResponseEntity<?> validateZipcode(String zipcode)
+    {
+        if (zipcode == null || zipcode.isEmpty()) {
+            return ResponseEntity.badRequest().body("Missing Zip Code");
+        }
+
+        //* check for out-of-country zips
+        for (char c : zipcode.toCharArray()) {
+            if (!Character.isDigit(c) || c == '-') {
+                return ResponseEntity.badRequest().body("Zip Code has invalid characters");
+            }
+        }
+
+        if (zipcode.length() < 5) {
+            return ResponseEntity.badRequest().body("Zip Code is too short");
+        }
+
+        if (zipcode.length() > 5) {
+            return ResponseEntity.badRequest().body("Zip Code is too long");
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/test/java/com/platner/cayuseapp/CayuseappApplicationIntegrationTests.java
+++ src/test/java/com/platner/cayuseapp/CayuseappApplicationIntegrationTests.java
@@ -11,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * POST, PUT and DELETE are not tested, since they shouldn't exist.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CayuseappApplicationIntegrationTests
@@ -22,72 +25,71 @@ public class CayuseappApplicationIntegrationTests
     private TestRestTemplate restTemplate;
 
     @Test
-    public void invalidZip_noZip_failsWith400()
+    public void invalidZip_noZip_failsWithBadRequest()
     {
-        TestRestTemplate rest = new TestRestTemplate();
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather", port), String.class);
-        Assert.assertEquals(HttpStatus.BAD_REQUEST, s.getStatusCode());
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather", port), String.class);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
     @Test
-    public void invalidZip_tooShortZip_failsWith400()
+    public void invalidZip_tooShortZip_failsWithBadRequest()
     {
-        TestRestTemplate rest = new TestRestTemplate();
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=9700", port), String.class);
-        Assert.assertEquals(HttpStatus.BAD_REQUEST, s.getStatusCode());
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=9700", port), String.class);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
 
+    /**
+     * This zip is somewhere in Boston, but the OpenWeatherMap API doesn't seem to support Zip+4
+     */
+    @Test
+    public void invalidZip_tooLongZip_failsWithBadRequest()
+    {
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=02129-3011", port), String.class);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    /**
+     * Post code for London without country identifier.
+     */
+    @Test
+    public void invalidZip_nonUsZip_failsWithBadRequest()
+    {
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=WC2N+5DU", port), String.class);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    /**
+     * This test has a zip code that is a valid length, but doesn't exist on the westher server.
+     */
+    @Test
+    public void invalidZip_zipDoesntExist_failsWithNotFound()
+    {
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=90000", port), String.class);
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
-    public void invalidZip_tooLongZip_failsWith400()
+    public void valid_5DigitZip_succeeds_1()
     {
-        TestRestTemplate rest = new TestRestTemplate();
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=97006-00011", port), String.class);
-        Assert.assertEquals(HttpStatus.BAD_REQUEST, s.getStatusCode());
-
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=97006", port), String.class);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
-    public void invalidZip_nonUsZip_failsWith404()
+    public void valid_5DigitZip_succeeds_2()
     {
-        TestRestTemplate rest = new TestRestTemplate();
-        //* Post code for London
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=WC2N+5DU", port), String.class);
-        Assert.assertEquals(HttpStatus.NOT_FOUND, s.getStatusCode());
-
+        //* Somewhere in Boston
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=02129", port), String.class);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    /**
+     * Post code for London with country identifier.
+     */
     @Test
-    public void invalidZip_zipDoesntExist_failsWith404()
+    public void valid_nonUsZip_succeeds()
     {
-        TestRestTemplate rest = new TestRestTemplate();
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=90000", port), String.class);
-        Assert.assertEquals(HttpStatus.NOT_FOUND, s.getStatusCode());
-
-    }
-
-    //* POST returns error
-    //* PUT returns error
-    //* DELETE returns error
-
-    //* Valid tests
-    @Test
-    public void valid_zip_succeeds()
-    {
-        TestRestTemplate rest = new TestRestTemplate();
-
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=97006", port), String.class);
-
-        Assert.assertEquals(HttpStatus.OK, s.getStatusCode());
-    }
-
-    @Test
-    public void valid_zip4_succeeds()
-    {
-        TestRestTemplate rest = new TestRestTemplate();
-
-        ResponseEntity<String> s = rest.getForEntity(String.format("http://localhost:%d/weather?zipcode=970060001", port), String.class);
-
-        Assert.assertEquals(HttpStatus.OK, s.getStatusCode());
+        ResponseEntity<?> response = restTemplate.getForEntity(String.format("http://localhost:%d/weather?zipcode=WC2N+5DU,uk", port), String.class);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
